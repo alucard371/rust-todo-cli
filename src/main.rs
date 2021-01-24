3d0a40768332a7a69066c4c0dfc0cf99cd8fc654
Balohé <balohe37pro@gmail.com>
2021-01-24 11:54:28 +0100

    How to use struct in main

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
-use std::collections::HashMap
+use std::collections::HashMap;
 
 fn main() {
     let action = std::env::args().nth(1).expect("Please specify an action");
     let item = std::env::args().nth(2).expect("Please specify an item");
     
     println!("{:?}, {:?}", action, item);
-    
+
+    let mut todo = Todo {
+        map: HashMap::new(),
+    };
+    if action == "add" {
+        todo.insert(item);
+        match todo.save() {
+            Ok(_) => println!("todo saved"),
+            Err(why) => println!("An error occurred: {}", why),
+        }
+    }
 }
 
 struct Todo {
