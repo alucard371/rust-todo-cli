932d38933beba420ef070247c7a3e2f31d33e2d5
Balohé <balohe37pro@gmail.com>
2021-01-24 13:18:15 +0100

    How to read from file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::collections::HashMap;
+use std::io::Read;
+use std::str::FromStr;
 
 fn main() {
     let action = std::env::args().nth(1).expect("Please specify an action");
@@ -6,9 +8,12 @@ fn main() {
     
     println!("{:?}, {:?}", action, item);
 
-    let mut todo = Todo {
+    /* let mut todo = Todo {
         map: HashMap::new(),
-    };
+    }; */
+
+    let mut todo = Todo::new().expect("Initialisation of db failed");
+
     if action == "add" {
         todo.insert(item);
         match todo.save() {
@@ -24,6 +29,42 @@ struct Todo {
 }
 
 impl Todo {
+fn new() -> Result<Todo, std::io::Error> {
+    // open the db file
+    let mut f = std::fs::OpenOptions::new()
+        .write(true)
+        .create(true)
+        .read(true)
+        .open("db.txt")?;
+    // read its content into a string
+    let mut content = String::new();
+    f.read_to_string(&mut content)?;
+
+    // allocate an empty HashMap
+    let mut map = HashMap::new();
+
+    // loop over each lines of the file with a for loop
+    for entries in content.lines() {
+        // split and bind values
+        let mut values = entries.split('\t');
+        let key = values.next().expect("No Key");
+        let val = values.next().expect("No Value");
+        // insert them into HashMap
+        map.insert(String::from(key), bool::from_str(val).unwrap());
+    }
+    
+    /* // loop over each lines of the file with map
+    let map: HashMap<String, bool> = content
+        .lines()
+        .map(|line|line.splitn(2, '\t').collect::<Vec<&str>>())
+        .map(|v| (v[0], v[1]))
+        .map(|(k, v)| (String::from(k), bool::from_str(v).unwrap()))
+        .collect();
+ */
+    // return Ok
+    Ok(Todo { map })
+}
+
     fn insert(&mut self, key:String) {
         // insert a new item into our map
         // We pass true as value
